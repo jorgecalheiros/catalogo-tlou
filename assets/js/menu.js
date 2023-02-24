5fceee8634b522c93738d81dea8c637357c7aa3a
Jorge Calheiros <jorgecalheiros.s@gmail.com>
2023-02-24 13:43:42 -0300

    ajustes

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,37 @@
 const menu = document.querySelector("#menu-icon");
+const header = document.querySelector("#header");
+const colorWhite = "header-white";
+const colorTransparent = "header-transparent";
 menu.addEventListener("click", () => {
   const classHiddenNavBar = "hidden-navbar";
   const navbarmenu = document.querySelector("#navbarmenu");
   if (!navbarmenu.classList.contains(classHiddenNavBar)) {
+    if (window.scrollY == 0) {
+      header.classList.remove(colorWhite);
+    }
     navbarmenu.classList.add(classHiddenNavBar);
   } else {
     navbarmenu.classList.remove(classHiddenNavBar);
+    header.classList.add(colorWhite);
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const Y = window.scrollY;
+
+  if (Y == 0) {
+    if (header.classList.contains(colorWhite)) {
+      header.classList.remove(colorWhite);
+    }
+    if (!header.classList.contains(colorTransparent)) {
+      header.classList.add(colorTransparent);
+    }
+  } else {
+    if (header.classList.contains(colorTransparent)) {
+      header.classList.remove(colorTransparent);
+    }
+    if (!header.classList.contains(colorWhite)) {
+      header.classList.add(colorWhite);
+    }
   }
 });
